48b592033d995b0579c231c6357b8ef589c545c7
ASM <cpp.create@gmail.com>
2015-05-14 01:32:06 +0300

    Add constants, trow additional exceptions, fix typos

--- src/bsb_io.pyx
+++ src/bsb_io.pyx
@@ -4,22 +4,33 @@ cdef extern from "bsb_fastio.h":
     void bsb_set(int gpio, int value)
     int bsb_read(int gpio)
 
+INPUT = 0
+OUTPUT = 1
+HIGH = 1
+LOW = 0
+ON = 1
+OFF = 0
 
 cdef class Pin:
     cdef int gpio
     def __cinit__(self, int gpio):
-        int res = bsb_setup()
+        cdef int res = bsb_setup()
         if res!=0:
             raise RuntimeError("Something went wrong with GPIO setup:", res)
         self.gpio = gpio
 
-    def setDirection(self, int direction):
-        if direction in [0,1]:
-            bsb_direction(self.gpio, direction)
-        else:
-            raise RuntimeError(
-                "Direction can only be 1 or 0, specified:"+str(direction))
+    property direction:
+        def __set__(self, int direction):
+            if direction in [0,1]:
+                bsb_direction(self.gpio, direction)
+            else:
+                raise RuntimeError(
+                    "Direction can only be 1 or 0, specified:"+str(direction))
 
     property value:
             def __get__(self): return bsb_read(self.gpio)
-            def __set__(self, val): bsb_set(self.gpio, val)
+            def __set__(self, val):
+                if val in [0,1]:
+                    bsb_set(self.gpio, val)
+                else:
+                    raise RuntimeError("Can't assign value "+str(val))
